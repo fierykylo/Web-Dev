a058740bdf843ed80f1e0d99a4abcf7227df2758
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-04-26 11:25:57 +0530

    functions and methods in js

---
+++ javascript/functions/fun11.js
@@ -0,0 +1,10 @@
+const square = {
+    area(side)
+    {
+        return side*side;
+    },
+    perimeter(side)
+    {
+        return 4 * side;
+    }
+};
---
+++ javascript/functions/fun12.js
@@ -0,0 +1,8 @@
+const hen = {
+    name: "helen",
+    eggCount: 0,
+    layAnEgg() {
+        this.eggCount++;
+        console.log("EGG");
+    }
+};
