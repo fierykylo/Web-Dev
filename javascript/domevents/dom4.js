fb953d3916a4e5cd6457b3975f3cc7190cfd185a
Aarush Dubey <mrknownunknown2702@gmail.com>
2026-04-26 21:59:57 +0530

    dom attribute selector

---
+++ javascript/domevents/dom4.js
@@ -0,0 +1,4 @@
+// https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200
+const image = document.querySelector("img");
+image.src = "https://images.unsplash.com/photo-1581061090142-c2cd0ec9f021?w=200";
+image.alt = "chicken";
